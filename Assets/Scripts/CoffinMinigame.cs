20558cdef780e3f8e8f15abb514b1710a0cf0430
OfficialFoneE <OfficialFoneE@gmail.com>
2022-04-17 09:04:53 -0700

    Coffin Commit

--- Assets/Scripts/CoffinMinigame.cs
+++ Assets/Scripts/CoffinMinigame.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CoffinMinigame : MonoBehaviour
 {
-    public CoffinSelection[] coffinSelectionScripts = new CoffinSelection[5];
+    public CoffinSelection[] coffinSelectionScripts;
 
     private bool isSolved = false;
 
