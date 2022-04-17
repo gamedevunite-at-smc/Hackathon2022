95fecb7f2286ef44d267ba4d66e43403e18b5a14
OfficialFoneE <OfficialFoneE@gmail.com>
2022-04-17 08:39:06 -0700

    Coffin Minigame

---
+++ Assets/Scripts/CoffinMinigame.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffinMinigame : MonoBehaviour
+{
+    public CoffinSelection[] coffinSelectionScripts = new CoffinSelection[5];
+
+    private bool isSolved = false;
+
+    private void Awake()
+    {
+        coffinSelectionScripts = GetComponentsInChildren<CoffinSelection>();
+    }
+
+    void Update()
+    {
+
+        if (isSolved) return;
+
+        for (int i = 0; i < coffinSelectionScripts.Length; i++)
+        {
+            if(coffinSelectionScripts[i].isSet != true)
+            {
+                return;
+            }
+        }
+        EndMenuScript.CompleteMinigame();
+
+        isSolved = true;
+    }
+}
